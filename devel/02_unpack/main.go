47b9c19f1d8ac0e0993465e713fa88924892e357
Pumpkin-A <90926986+Pumpkin-A@users.noreply.github.com>
2025-01-05 02:03:57 +0300

    unpacker finish

--- devel/02_unpack/main.go
+++ devel/02_unpack/main.go
@@ -8,148 +8,142 @@ import (
 
 const backslashCode = 92
 
-func main() {
+type Unpacker struct {
+	//контекст
+	Symbol    rune
+	Len       int
+	Backslash bool
 
+	builder strings.Builder
 }
 
-type unpacker struct {
-	ctxLetter    rune
-	ctxLen       int
-	ctxBackslash bool
-	builder      strings.Builder
-}
-
-func newUnpacker() *unpacker {
-	return &unpacker{
+func NewUnpacker() *Unpacker {
+	return &Unpacker{
 		builder: strings.Builder{},
 	}
 }
 
-// naming!!
-func (u *unpacker) isCtxLetterEmpty() bool {
-	var empty rune
-	return u.ctxLetter == empty
+func (u *Unpacker) IsSymbolEmpty() bool {
+	return u.Symbol == rune(0)
 }
 
-func (u *unpacker) isCtxBackslash() bool {
-	return u.ctxBackslash
+func (u *Unpacker) IsBackslash() bool {
+	return u.Backslash
 }
 
-func (u *unpacker) updateCtxLetter(newChar rune) {
-	u.ctxLetter = newChar
+func (u *Unpacker) UpdateSymbol(newChar rune) {
+	u.Symbol = newChar
 }
 
-func (u *unpacker) updateCtxLen(newLen int) {
-	u.ctxLen = u.ctxLen*10 + newLen
+func (u *Unpacker) UpdateLen(newLen int) {
+	u.Len = u.Len*10 + newLen
 }
 
-func (u *unpacker) updateCtxBackslash() {
-	u.ctxBackslash = true
+func (u *Unpacker) UpdateBackslash() {
+	u.Backslash = true
 }
 
-func (u *unpacker) resetCtx() {
-	var empty rune
-	u.ctxLetter = empty
-	u.ctxLen = 0
-	u.ctxBackslash = false
+func (u *Unpacker) ResetCtx() {
+	u.Symbol = rune(0)
+	u.Len = 0
+	u.Backslash = false
 }
 
 // записывает контекст в итоговую строку указанное количество раз
-func (u *unpacker) writeCtxToString() {
+func (u *Unpacker) WriteCtxToString() {
 	var len int
-	if u.ctxLen == 0 {
+	if u.Len == 0 {
 		len = 1
 	} else {
-		len = u.ctxLen
+		len = u.Len
 	}
-	u.builder.WriteString(strings.Repeat(string(u.ctxLetter), len))
+	u.builder.WriteString(strings.Repeat(string(u.Symbol), len))
 	// очистка контекста после записи
-	u.resetCtx()
+	u.ResetCtx()
 }
 
-func (u *unpacker) lettersHandler(r rune) error {
+func (u *Unpacker) SymbolsHandler(r rune) error {
 	// встретили новый символ буквы. Если контекст не пуст - надо записать данные в итоговую строку
 	// После обработки записываем в контекст новый символ
-	if !u.isCtxLetterEmpty() {
-		u.writeCtxToString()
-	} else if u.isCtxBackslash() {
+	if !u.IsSymbolEmpty() {
+		u.WriteCtxToString()
+	} else if u.IsBackslash() {
 		return fmt.Errorf("incorrect string: unused backslash")
 	}
-	u.updateCtxLetter(r)
+	u.UpdateSymbol(r)
 	return nil
 }
 
-func (u *unpacker) digitsHandler(len rune) error {
+func (u *Unpacker) DigitsHandler(len rune) error {
 	// если контекст пуст и не включен контекст слэша - ошибка (нет символов для повтора)
 	// если контекст слэша включен, значит цифра - символ для обработки
-	if u.isCtxLetterEmpty() {
-		if u.isCtxBackslash() {
-			u.updateCtxLetter(len)
+	if u.IsSymbolEmpty() {
+		if u.IsBackslash() {
+			u.UpdateSymbol(len)
 			return nil
 		}
 		return fmt.Errorf("incorrect string")
 	}
 	// встретили очередную цифру в строке - обновляем длину (это длина символа, который сейчас в контексте)
-	u.updateCtxLen(int(len - '0'))
+	u.UpdateLen(int(len - '0'))
 	return nil
 }
 
-func (u *unpacker) backslashHandler() error {
+func (u *Unpacker) BackslashHandler() error {
 	// если контекст слэша включен и символа для обработки нет - слэш и есть символ для обработки
 	// если контекст слэша включен и контекст символа заполнен - значит записываем накопленный контекст в итоговую строку
-	if u.isCtxBackslash() {
-		if u.isCtxLetterEmpty() {
-			u.updateCtxLetter(rune(backslashCode))
+	if u.IsBackslash() {
+		if u.IsSymbolEmpty() {
+			u.UpdateSymbol(rune(backslashCode))
 			return nil
 		}
-		u.writeCtxToString()
-		u.updateCtxBackslash()
+		u.WriteCtxToString()
+		u.UpdateBackslash()
 		return nil
 	}
 
-	if !u.isCtxLetterEmpty() {
-		u.writeCtxToString()
+	if !u.IsSymbolEmpty() {
+		u.WriteCtxToString()
 	}
-	u.updateCtxBackslash()
+	u.UpdateBackslash()
 	return nil
 }
 
-func unpacking(str string) (string, error) {
-	fmt.Println(str)
-	unpacker := newUnpacker()
-	runes := []rune(str)
-
-	for _, v := range runes {
+func (u *Unpacker) Do(str string) (string, error) {
+	for _, v := range str {
 		if unicode.IsDigit(v) {
 			// обработчик, когда встречаем число
-			err := unpacker.digitsHandler(v)
+			err := u.DigitsHandler(v)
 			if err != nil {
 				return "", err
 			}
 			continue
 		} else if int(v) == backslashCode {
 			// обработчик после слэша
-			err := unpacker.backslashHandler()
+			err := u.BackslashHandler()
 			if err != nil {
 				return "", err
 			}
 			continue
 		}
 
-		err := unpacker.lettersHandler(v)
+		err := u.SymbolsHandler(v)
 		if err != nil {
 			return "", err
 		}
-
 	}
 
 	//обработка накопленного контекста после завершения строки
-	if !unpacker.isCtxLetterEmpty() {
-		unpacker.writeCtxToString()
-	} else if unpacker.ctxBackslash {
+	if !u.IsSymbolEmpty() {
+		u.WriteCtxToString()
+	} else if u.Backslash {
 		return "", fmt.Errorf("incorrect string: unused backslash")
 	}
 
-	fmt.Println(unpacker.builder.String())
-	return unpacker.builder.String(), nil
+	fmt.Println(u.builder.String())
+	return u.builder.String(), nil
+}
+
+func main() {
+
 }
--- devel/02_unpack/main_test.go
+++ devel/02_unpack/main_test.go
@@ -7,95 +7,111 @@ func Test_unpacking(t *testing.T) {
 		str string
 	}
 	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
+		name     string
+		unpacker *Unpacker
+		args     args
+		want     string
+		wantErr  bool
 	}{
 		{
-			name: "OK - Default",
-			args: args{str: "a4bc2d5e"},
-			want: "aaaabccddddde",
+			name:     "OK - Default",
+			unpacker: NewUnpacker(),
+			args:     args{str: "a4bc2d5e"},
+			want:     "aaaabccddddde",
 		},
 		{
-			name: "Ok - long numbers",
-			args: args{str: "a100b50"},
-			want: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+			name:     "Ok - long numbers",
+			unpacker: NewUnpacker(),
+			args:     args{str: "a100b50"},
+			want:     "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
 		},
 		{
-			name: "Ok - No repeat",
-			args: args{str: "abcd"},
-			want: "abcd",
+			name:     "Ok - No repeat",
+			unpacker: NewUnpacker(),
+			args:     args{str: "abcd"},
+			want:     "abcd",
 		},
 		{
-			name: "Ok - with unicode",
-			args: args{str: "a5한3b2"},
-			want: "aaaaa한한한bb",
+			name:     "Ok - with unicode",
+			unpacker: NewUnpacker(),
+			args:     args{str: "a5한3b2"},
+			want:     "aaaaa한한한bb",
 		},
 		{
-			name: "Empty string",
-			args: args{str: ""},
-			want: "",
+			name:     "Empty string",
+			unpacker: NewUnpacker(),
+			args:     args{str: ""},
+			want:     "",
 		},
 		{
-			name:    "Error string - only numbers",
-			args:    args{str: "123"},
-			want:    "",
-			wantErr: true,
+			name:     "Error string - only numbers",
+			unpacker: NewUnpacker(),
+			args:     args{str: "123"},
+			want:     "",
+			wantErr:  true,
 		},
 		{
-			name:    "Error string - first number",
-			args:    args{str: "12ab5d5"},
-			want:    "",
-			wantErr: true,
+			name:     "Error string - first number",
+			unpacker: NewUnpacker(),
+			args:     args{str: "12ab5d5"},
+			want:     "",
+			wantErr:  true,
 		},
 		{
-			name: "non-number or word chars",
-			args: args{str: "a5d5f2.3d"},
-			want: "aaaaadddddff...d",
+			name:     "non-number or word chars",
+			unpacker: NewUnpacker(),
+			args:     args{str: "a5d5f2.3d"},
+			want:     "aaaaadddddff...d",
 		},
 		{
-			name: "Escape - 1",
-			args: args{str: `qwe\4\5`},
-			want: "qwe45",
+			name:     "Escape - 1",
+			unpacker: NewUnpacker(),
+			args:     args{str: `qwe\4\5`},
+			want:     "qwe45",
 		},
 		{
-			name: "Escape - 2",
-			args: args{str: `qwe\45`},
-			want: "qwe44444",
+			name:     "Escape - 2",
+			unpacker: NewUnpacker(),
+			args:     args{str: `qwe\45`},
+			want:     "qwe44444",
 		},
 		{
-			name: "Escape - 3",
-			args: args{str: `qwe\\5`},
-			want: `qwe\\\\\`,
+			name:     "Escape - 3",
+			unpacker: NewUnpacker(),
+			args:     args{str: `qwe\\5`},
+			want:     `qwe\\\\\`,
 		},
 		{
-			name:    "Escape - 4",
-			args:    args{str: `qwe\`},
-			want:    "",
-			wantErr: true,
+			name:     "Escape - 4",
+			unpacker: NewUnpacker(),
+			args:     args{str: `qwe\`},
+			want:     "",
+			wantErr:  true,
 		},
 		{
-			name:    "Escape - 5",
-			args:    args{str: `\\\`},
-			want:    "",
-			wantErr: true,
+			name:     "Escape - 5",
+			unpacker: NewUnpacker(),
+			args:     args{str: `\\\`},
+			want:     "",
+			wantErr:  true,
 		},
 		{
-			name: "Escape - 6",
-			args: args{str: `\\\\a`},
-			want: `\\a`,
+			name:     "Escape - 6",
+			unpacker: NewUnpacker(),
+			args:     args{str: `\\\\a`},
+			want:     `\\a`,
 		},
 		{
-			name:    "Escape - 7",
-			args:    args{str: `\f`},
-			want:    "",
-			wantErr: true,
+			name:     "Escape - 7",
+			unpacker: NewUnpacker(),
+			args:     args{str: `\f`},
+			want:     "",
+			wantErr:  true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := unpacking(tt.args.str)
+			got, err := tt.unpacker.Do(tt.args.str)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("unpacking() error = %v, wantErr %v", err, tt.wantErr)
 				return
